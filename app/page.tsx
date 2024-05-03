9c9274451d348d725d20b5e063ccdf95869527d7
John Mathis <johnmathis98@gmail.com>
2024-05-03 11:33:53 -0400

    Root page edits. Added tailwind styling to the containing div.

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,10 @@ import WelcomeHeader from "./components/WelcomeHeader/WelcomeHeader";
 
 export default function Home() {
   return (
-    <main className=" h-screen bg-cover bg-no-repeat bg-left-top bg-[url(/pattern-background-mobile-light.svg)] dark:bg-[url(/pattern-background-mobile-dark.svg)]">
-        <ToggleMode/>
-        <WelcomeHeader/>
-        <MenuSelection/>
+    <main className=" h-screen bg-cover bg-no-repeat bg-left-top bg-[url(/pattern-background-mobile-light.svg)] bg-[--light-grey] dark:bg-[--dark-navy] dark:bg-[url(/pattern-background-mobile-dark.svg)]">
+      <ToggleMode />
+      <WelcomeHeader />
+      <MenuSelection />
     </main>
   );
 }
-        
