72260dc278d279be7494a7c20826743528dffa44
John Mathis <johnmathis98@gmail.com>
2024-05-03 17:55:11 -0400

    Responsive design edits on the Selection and Welcome Headers component. Added some breakpoints for the tablet screen size. #2

--- app/components/WelcomeHeader/WelcomeHeader.tsx
+++ app/components/WelcomeHeader/WelcomeHeader.tsx
@@ -1,7 +1,7 @@
 export default function WelcomeHeader() {
   return (
-    <section className="pt-20   mx-auto text-left w-[20.43rem]">
-      <h1 className="text-4xl">
+    <section className="pt-20 mx-auto text-left w-[20.43rem] md:w-[40rem]">
+      <h1 className="text-4xl md:w-[25rem]">
         Welcome to the <span className="font-semibold">Frontend Quiz!</span>{" "}
       </h1>
       <p className="mt-4 mb-8 italic text-[--grey-navy] dark:text-[--light-blue]">
