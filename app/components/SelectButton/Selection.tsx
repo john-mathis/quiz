b89e6e4727fa09b75ba3d34da3655c63a3344068
John Mathis <johnmathis98@gmail.com>
2024-05-03 17:51:26 -0400

    Responsive design edits on the Selection and Welcome Headers component. Added some breakpoints for the tablet screen size.

--- app/components/SelectButton/Selection.tsx
+++ app/components/SelectButton/Selection.tsx
@@ -1,17 +1,15 @@
-import Image from "next/image"
+import Image from "next/image";
 
-
-interface SelectionProps{
-img:string,
-title:string
+interface SelectionProps {
+  img: string;
+  title: string;
 }
 
-
-export default function Selection({img,title}: SelectionProps){
-return(
-    <div className=" flex justify-start items-center px-3 w-[20rem] h-16 bg-white dark:bg-[--grey-navy] rounded-lg text-white cursor-pointer">
-        <Image src={img} alt="" height={40} width={40}/>
-        <p className="ml-4 text-[--dark-navy] dark:text-white">{title}</p>
+export default function Selection({ img, title }: SelectionProps) {
+  return (
+    <div className=" flex justify-start items-center px-3 w-[20rem] md:w-[40rem] h-16 bg-white dark:bg-[--grey-navy] rounded-lg text-white cursor-pointer">
+      <Image src={img} alt="" height={40} width={40} />
+      <p className="ml-4 text-[--dark-navy] dark:text-white">{title}</p>
     </div>
-)
+  );
 }
