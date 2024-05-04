7ccedcf1c167365c9ce2698b76e5d25ba314a599
John Mathis <johnmathis98@gmail.com>
2024-05-03 21:28:02 -0400

    Responsive design based styling edits. Added additional break point styling to root page, Welcome header component, Selection component, and tailwind config file

--- app/components/WelcomeHeader/WelcomeHeader.tsx
+++ app/components/WelcomeHeader/WelcomeHeader.tsx
@@ -1,7 +1,7 @@
 export default function WelcomeHeader() {
   return (
-    <section className="pt-20 mx-auto text-left w-[20.43rem] md:w-[40rem]">
-      <h1 className="text-4xl md:w-[25rem]">
+    <section className="pt-20 mx-auto text-left w-[20.43rem] xs:w-[15rem] md:w-[40rem]">
+      <h1 className="text-4xl  xs:text-2xl md:w-[25rem]">
         Welcome to the <span className="font-semibold">Frontend Quiz!</span>{" "}
       </h1>
       <p className="mt-4 mb-8 italic text-[--grey-navy] dark:text-[--light-blue]">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,17 +6,26 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode:'class',
+  darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-          "light-mobile":"/pattern-background-mobile-light.svg"
+        "light-mobile": "/pattern-background-mobile-light.svg",
       },
     },
+    screens: {
+      xs: { max: "375px" },
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
+
   plugins: [],
 };
 export default config;
