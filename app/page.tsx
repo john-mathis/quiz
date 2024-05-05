7589cd17bbec61d53379715a37547bf19436c205
John Mathis <johnmathis98@gmail.com>
2024-05-05 19:54:40 -0400

    Root page responsive design edits

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import WelcomeHeader from "./components/WelcomeHeader/WelcomeHeader";
 
 export default function Home() {
   return (
-    <main className="h-screen bg-cover bg-no-repeat bg-left-top bg-[url(/pattern-background-mobile-light.svg)] bg-[--light-blue] dark:bg-[--dark-navy] dark:bg-[url(/pattern-background-mobile-dark.svg)] md:bg-[url(/pattern-background-tablet-light.svg)] dark:md:bg-[url(/pattern-background-tablet-dark.svg)] md:bg-auto">
+    <main className="h-screen bg-cover bg-no-repeat bg-left-top bg-[url(/pattern-background-mobile-light.svg)] bg-[--light-grey] dark:bg-[--dark-navy] dark:bg-[url(/pattern-background-mobile-dark.svg)] md:bg-[url(/pattern-background-tablet-light.svg)] dark:md:bg-[url(/pattern-background-tablet-dark.svg)] md:bg-auto lg:flex ">
       <ToggleMode />
       <WelcomeHeader />
       <MenuSelection />
