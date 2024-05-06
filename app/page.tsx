6a17e1fa96e78789f0e8befb58dda54d0cf2c63e
John Mathis <johnmathis98@gmail.com>
2024-05-05 22:42:54 -0400

    Root page desktop responsive design edits

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import WelcomeHeader from "./components/WelcomeHeader/WelcomeHeader";
 
 export default function Home() {
   return (
-    <main className="h-screen bg-cover bg-no-repeat bg-left-top bg-[url(/pattern-background-mobile-light.svg)] bg-[--light-grey] dark:bg-[--dark-navy] dark:bg-[url(/pattern-background-mobile-dark.svg)] md:bg-[url(/pattern-background-tablet-light.svg)] dark:md:bg-[url(/pattern-background-tablet-dark.svg)] md:bg-auto lg:flex lg:items-center lg:px-20">
+    <main className="h-screen bg-cover bg-no-repeat bg-left-top bg-[url(/pattern-background-mobile-light.svg)] bg-[--light-grey] dark:bg-[--dark-navy] dark:bg-[url(/pattern-background-mobile-dark.svg)] md:bg-[url(/pattern-background-tablet-light.svg)] dark:md:bg-[url(/pattern-background-tablet-dark.svg)] md:bg-auto lg:flex lg:items-center lg:px-20 lg:bg-[url(/pattern-background-desktop-light.svg)] dark:lg:bg-[url(/pattern-background-desktop-dark.svg)] lg:bg-center lg:bg-cover">
       <WelcomeHeader />
       <MenuSelection />
     </main>
