a1205f0477aedc31325cee372fdef67bb0d63582
John  Mathis <johnmathis98@gmail.com>
2024-05-08 09:36:21 -0400

    Added links to the Menu Selection Component. A click event will now direct the user to the Question Page page.

--- app/components/MenuSelection/MenuSelection.tsx
+++ app/components/MenuSelection/MenuSelection.tsx
@@ -1,12 +1,13 @@
 import Selection from "../SelectButton/Selection";
+import Link from "next/link";
 
 export default function MenuSelection() {
   return (
     <section className="flex flex-col justify-between items-center h-[18.25rem] ">
-      <Selection img={"/icon-html.svg"} title={"HTML"} />
-      <Selection img={"/icon-css.svg"} title={"CSS"} />
-      <Selection img={"/icon-js.svg"} title={"Javascript"} />
-      <Selection img={"/icon-accessibility.svg"} title={"Accessibility"} />
+      <Link href={"/QuestionPage"}><Selection img={"/icon-html.svg"} title={"HTML"} /></Link>
+      <Link href={"/QuestionPage"}><Selection img={"/icon-css.svg"} title={"CSS"} /></Link>
+      <Link href={"/QuestionPage"}><Selection img={"/icon-js.svg"} title={"Javascript"} /></Link>
+      <Link href={"/QuestionPage"}><Selection img={"/icon-accessibility.svg"} title={"Accessibility"} /></Link>
     </section>
   );
 }
