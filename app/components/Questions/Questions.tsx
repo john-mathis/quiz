0d69185cd10110607264341bfd69426cbdc0c96a
John  Mathis <johnmathis98@gmail.com>
2024-05-08 11:23:46 -0400

    Changed the image path within the JSON file. Added temporary logic displaying HTML questions and answer options. Also added logic to incrementally proceed to the next question.

--- app/components/Questions/Questions.tsx
+++ app/components/Questions/Questions.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { useState } from "react";
 import Data from "../../data.json";
+import Image from "next/image";
 
 interface QuestionProps {
   question: string;
@@ -7,22 +9,46 @@ interface QuestionProps {
   answerChoice: string;
 }
 
-export default function Answers({
+export default function Questions({
   question,
   letterChoice,
   answerChoice,
 }: QuestionProps) {
-  const HTML_DATA = Data.quizzes[0].questions;
-  console.log(HTML_DATA);
+
+  const [currentQuestionIndex, setCurrentQuestionIndex] =useState(0)
+
+  const handleNext = ()=>{
+    if(currentQuestionIndex < HTML_QUESTIONS.length-1 ){
+      setCurrentQuestionIndex(currentQuestionIndex + 1)
+    }
+  }
+
+  const HTML_DATA = Data.quizzes[0];
+  const HTML_QUESTIONS = Data.quizzes[0].questions
+  const HTML_OPTIONS = Data.quizzes[0].questions[currentQuestionIndex].options
+  
   return (
-    <section>
-      <div className=" flex justify-start items-center px-3 w-[20rem] xs:w-[15rem] md:w-[40rem] lg:w-[30.25rem] h-16 bg-white dark:bg-[--grey-navy] rounded-lg text-white cursor-pointer">
-        <p>Question {1} of 10 </p>
-        {HTML_DATA.map((a) => {
-          return <></>;
-        })}
-        <p className="ml-4 text-[--dark-navy] dark:text-white">{}</p>
+    <section className="mx-auto flex flex-col items-center">
+
+      <div>
+      <p>Question {currentQuestionIndex + 1} of 10 </p>
+      {HTML_QUESTIONS[currentQuestionIndex].question}
       </div>
+
+
+
+{/* flex flex-col justify-start items-center px-3 w-[20rem] xs:w-[15rem] md:w-[40rem] lg:w-[30.25rem]  bg-white dark:bg-[--grey-navy] rounded-lg text-white cursor-pointer */}
+      <div className="flex flex-col justify-center">
+        {HTML_OPTIONS.map((o)=>{
+          return(
+            <p key={o} className=" pl-4 h-16 w-[20.43rem] md:w-[40rem] lg:w-[30.25rem] mt-4 bg-white dark:bg-[--grey-navy] rounded-lg cursor-pointer">{o}</p>
+          )
+        })}
+        <button onClick={handleNext}>next</button>
+      </div>
+
+
+
     </section>
   );
 }
