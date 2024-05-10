941deb333c4143ba22660b78ef0bc6b48bd7d57f
John Mathis <johnmathis98@gmail.com>
2024-05-10 14:04:32 -0400

    Code formatting

--- app/components/MenuSelection/MenuSelection.tsx
+++ app/components/MenuSelection/MenuSelection.tsx
@@ -4,10 +4,18 @@ import Link from "next/link";
 export default function MenuSelection() {
   return (
     <section className="flex flex-col justify-between items-center h-[18.25rem] ">
-      <Link href={"/QuestionPage"}><Selection img={"/icon-html.svg"} title={"HTML"} /></Link>
-      <Link href={"/QuestionPage"}><Selection img={"/icon-css.svg"} title={"CSS"} /></Link>
-      <Link href={"/QuestionPage"}><Selection img={"/icon-js.svg"} title={"Javascript"} /></Link>
-      <Link href={"/QuestionPage"}><Selection img={"/icon-accessibility.svg"} title={"Accessibility"} /></Link>
+      <Link href={"/QuestionPage"}>
+        <Selection img={"/icon-html.svg"} title={"HTML"} />
+      </Link>
+      <Link href={"/QuestionPage"}>
+        <Selection img={"/icon-css.svg"} title={"CSS"} />
+      </Link>
+      <Link href={"/QuestionPage"}>
+        <Selection img={"/icon-js.svg"} title={"Javascript"} />
+      </Link>
+      <Link href={"/QuestionPage"}>
+        <Selection img={"/icon-accessibility.svg"} title={"Accessibility"} />
+      </Link>
     </section>
   );
 }
