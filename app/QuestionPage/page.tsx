f7d362359cb2ba7371c26c558f59570f36a1e8a2
John Mathis <johnmathis98@gmail.com>
2024-05-10 13:06:56 -0400

    Removed Submit Button component from Questions page and added it into the Questions Component. Also added responsive design styling.

--- app/QuestionPage/page.tsx
+++ app/QuestionPage/page.tsx
@@ -1,13 +1,17 @@
 import Questions from "../components/Questions/Questions";
-import SubmitButton from "../components/SubmitButton/SubmitButton";
+import QuizData from "../data.json";
 
 export default function question() {
-  
+  // Data.quizzes[]
+  // 0 - HTML
+  // 1 - CSS
+  // 2 - JavaScript
+  // 3 - Accessability
+  const HTML = QuizData.quizzes;
 
   return (
-    <div className="mx-auto flex flex-col items-center">
-      <Questions />
-      <SubmitButton />
+    <div className="mx-auto flex flex-col items-center justify-between xl:items-end xl:w-[72.5rem]">
+      <Questions QUESTIONS={HTML} OPTIONS={HTML} />
     </div>
   );
 }
