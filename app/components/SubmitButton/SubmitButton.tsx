9ad825af7a115089ebf713c63083614c116b2fef
John Mathis <johnmathis98@gmail.com>
2024-05-10 14:03:54 -0400

    Added logic which enables a function to be passed into the component as props.

--- app/components/SubmitButton/SubmitButton.tsx
+++ app/components/SubmitButton/SubmitButton.tsx
@@ -1,5 +1,14 @@
-export default function SubmitButton(){
-    return(
-        <button className="w-[20.43rem] md:w-[40rem] h-[3.5rem] md:h-[5.75rem] bg-[--purple] rounded-lg ">Submit Answer</button>
-    )
+interface SubmitButtonProps {
+  action: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function SubmitButton({ action }: SubmitButtonProps) {
+  return (
+    <button
+      onClick={action}
+      className="w-[20.43rem] md:w-[40rem] lg:w-[30.25rem] h-[3.5rem] md:h-[5.75rem] mt-6 bg-[--purple] rounded-lg "
+    >
+      Submit Answer
+    </button>
+  );
 }
