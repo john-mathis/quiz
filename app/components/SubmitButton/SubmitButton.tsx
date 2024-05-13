a72a9f17f967ec2470f925d7fb61ae64ec2623f7
John Mathis <johnmathis98@gmail.com>
2024-05-13 11:21:10 -0400

    Submit Button component formating edits

--- app/components/SubmitButton/SubmitButton.tsx
+++ app/components/SubmitButton/SubmitButton.tsx
@@ -6,7 +6,7 @@ export default function SubmitButton({ action }: SubmitButtonProps) {
   return (
     <button
       onClick={action}
-      className="w-[20.43rem] md:w-[40rem] lg:w-[30.25rem] h-[3.5rem] md:h-[5.75rem] mt-6 bg-[--purple] rounded-lg "
+      className="w-[20.43rem] md:w-[40rem] lg:w-[30.25rem] h-[3.5rem] md:h-[5.75rem] mt-6 bg-[--purple] rounded-lg text-white"
     >
       Submit Answer
     </button>
