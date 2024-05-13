604a6154d95ae497097fe5b1c353f6a8ae2ff2b0
John Mathis <johnmathis98@gmail.com>
2024-05-13 11:18:20 -0400

    Root page formating edits

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import WelcomeHeader from "./components/WelcomeHeader/WelcomeHeader";
 
 export default function Home() {
   return (
-    <main className="mx-auto lg:pt-64 lg:w-[55.5rem] lg:h-[28.5] md:bg-auto lg:flex lg:justify-between lg:items-center ">
+    <main className="mx-auto lg:pt-64 lg:w-[55.5rem] lg:h-[28.5] md:bg-auto lg:flex lg:justify-between lg:items-center xl:w-[72.5rem] ">
       <WelcomeHeader />
       <MenuSelection />
     </main>
