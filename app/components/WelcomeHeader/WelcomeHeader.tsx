427c69601d1f6d3eccf611d6845607f5d2d159fb
John Mathis <johnmathis98@gmail.com>
2024-05-13 11:19:51 -0400

    Welcome Header component Responsive design edits. Added in width for the XL screen size.

--- app/components/WelcomeHeader/WelcomeHeader.tsx
+++ app/components/WelcomeHeader/WelcomeHeader.tsx
@@ -1,6 +1,6 @@
 export default function WelcomeHeader() {
   return (
-    <section className="pt-20 mx-auto text-left w-[20.43rem] xs:w-[15rem] md:w-[40rem] lg:w-[29.06rem] lg:flex lg:flex-col lg:justify-start lg:pt-0 lg:h-[18.25rem]">
+    <section className="pt-20 mx-auto text-left w-[20.43rem] xs:w-[15rem] md:w-[40rem] lg:w-[29.06rem] lg:flex lg:flex-col lg:justify-start lg:pt-0 lg:h-[18.25rem] xl:w-[72.5rem]">
       <h1 className="text-4xl xs:text-2xl md:w-[25rem]">
         Welcome to the <span className="font-semibold">Frontend Quiz!</span>{" "}
       </h1>
