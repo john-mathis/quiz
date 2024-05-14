4c0b9a98c7e13ce8a533920133581da2abecb0fc
John Mathis <johnmathis98@gmail.com>
2024-05-13 20:42:47 -0400

    Styling formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "../providers";
 import ToggleMode from "./components/ToggleMode/ToggleMode";
+import { AppWrapper } from "./context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body className={inter.className + "h-screen bg-cover bg-no-repeat bg-left-top bg-[url(/pattern-background-mobile-light.svg)] bg-[--light-grey] dark:bg-[--dark-navy] dark:bg-[url(/pattern-background-mobile-dark.svg)] md:bg-[url(/pattern-background-tablet-light.svg)] dark:md:bg-[url(/pattern-background-tablet-dark.svg)]  lg:bg-[url(/pattern-background-desktop-light.svg)] dark:lg:bg-[url(/pattern-background-desktop-dark.svg)] lg:bg-center lg:bg-cover"}>
-        <Providers>
-          <ToggleMode/>
-          {children}</Providers>
+      <body
+        className={
+          inter.className +
+          "h-screen bg-cover bg-no-repeat bg-left-top bg-[url(/pattern-background-mobile-light.svg)] bg-[--light-grey] dark:bg-[--dark-navy] dark:bg-[url(/pattern-background-mobile-dark.svg)] md:bg-[url(/pattern-background-tablet-light.svg)] dark:md:bg-[url(/pattern-background-tablet-dark.svg)]  lg:bg-[url(/pattern-background-desktop-light.svg)] dark:lg:bg-[url(/pattern-background-desktop-dark.svg)] lg:bg-center lg:bg-cover"
+        }
+      >
+        <AppWrapper>
+          <Providers>
+            <ToggleMode />
+            {children}
+          </Providers>
+        </AppWrapper>
       </body>
     </html>
   );
