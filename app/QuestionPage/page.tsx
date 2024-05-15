3a0864bd80a57cc42d133691dca0243ef47fa92b
John  Mathis <johnmathis98@gmail.com>
2024-05-15 16:35:32 -0400

    Removed unused imports and comments.

--- app/QuestionPage/page.tsx
+++ app/QuestionPage/page.tsx
@@ -1,22 +1,13 @@
-"use client";
-import { useEffect } from "react";
+"use client"
 import { useAppContext } from "../context";
 import Questions from "../components/Questions/Questions";
-import QuizData from "../data.json";
+
 
 export default function Question() {
-  // Data.quizzes[]
-  // 0 - HTML
-  // 1 - CSS
-  // 2 - JavaScript
-  // 3 - Accessability
-  const HTML = QuizData.quizzes;
-
-  //This component will take the state which was set in the Menu Selection component and pass it to the questions component.
-
+  const {topic} = useAppContext()
   return (
     <div className="mx-auto flex flex-col items-center justify-between xl:items-end xl:w-[72.5rem]">
-      <Questions QUESTIONS={HTML} OPTIONS={HTML} />
+      <Questions QUESTIONS={topic} />
     </div>
   );
 }
