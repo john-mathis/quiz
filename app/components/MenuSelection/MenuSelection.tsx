7175f2ab650a02bc28a129e82b2eb03af00655ef
John  Mathis <johnmathis98@gmail.com>
2024-05-15 13:30:41 -0400

    Refactored code to make it more clean and efficient.

--- app/components/MenuSelection/MenuSelection.tsx
+++ app/components/MenuSelection/MenuSelection.tsx
@@ -3,31 +3,25 @@ import { useAppContext } from "@/app/context";
 import Selection from "../SelectButton/Selection";
 import Link from "next/link";
 import QUIZ_DATA from "../../data.json";
-export default function MenuSelection() {
-  const HTML_QUESTIONS = QUIZ_DATA.quizzes[0].questions;
-  const CSS_QUESTIONS = QUIZ_DATA.quizzes[1].questions;
-  const JAVASCRIPT_QUESTIONS = QUIZ_DATA.quizzes[2].questions;
-  const ACCESSABILITY_QUESTIONS = QUIZ_DATA.quizzes[3].questions;
 
+export default function MenuSelection() {
+  const { quizzes } = QUIZ_DATA;
   const { setTopic } = useAppContext();
 
-
-  
+  const quizTopics = [
+    { questions: quizzes[0].questions, img: "/icon-html.svg", title: "HTML" },
+    { questions: quizzes[1].questions, img: "/icon-css.svg", title: "CSS" },
+    { questions: quizzes[2].questions, img: "/icon-js.svg", title: "Javascript" },
+    { questions: quizzes[3].questions, img: "/icon-accessibility.svg", title: "Accessibility" }
+  ];
 
   return (
-    <section className="flex flex-col justify-between items-center h-[18.25rem] ">
-      <Link href={"/QuestionPage"} onClick={() => setTopic(HTML_QUESTIONS)}>
-        <Selection img={"/icon-html.svg"} title={"HTML"} />
-      </Link>
-      <Link href={"/QuestionPage"} onClick={() => setTopic(CSS_QUESTIONS)}>
-        <Selection img={"/icon-css.svg"} title={"CSS"} />
-      </Link>
-      <Link href={"/QuestionPage"} onClick={() => setTopic(JAVASCRIPT_QUESTIONS)}>
-        <Selection img={"/icon-js.svg"} title={"Javascript"} />
-      </Link>
-      <Link href={"/QuestionPage"} onClick={() => setTopic(ACCESSABILITY_QUESTIONS)}>
-        <Selection img={"/icon-accessibility.svg"} title={"Accessibility"} />
-      </Link>
+    <section className="flex flex-col justify-between items-center h-[18.25rem]">
+      {quizTopics.map((quiz, index) => (
+        <Link href="/QuestionPage" key={index} onClick={() => setTopic(quiz.questions)}>
+          <Selection img={quiz.img} title={quiz.title} />
+        </Link>
+      ))}
     </section>
   );
 }
