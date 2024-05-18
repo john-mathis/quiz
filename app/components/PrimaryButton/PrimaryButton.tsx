dd11a04d6b9ebf4589123a28717c1fd0395b4288
John Mathis <johnmathis98@gmail.com>
2024-05-18 11:19:19 -0400

    Rename SubmitButton to PrimaryButton and add text props
    - Renamed the SubmitButton component to PrimaryButton for clarity.
    - Added props to the PrimaryButton component to dynamically display button text.

--- app/components/PrimaryButton/PrimaryButton.tsx
+++ app/components/PrimaryButton/PrimaryButton.tsx
@@ -1,14 +1,15 @@
-interface SubmitButtonProps {
+interface PrimaryButtonProps {
   action: React.MouseEventHandler<HTMLButtonElement>;
+  text: string;
 }
 
-export default function SubmitButton({ action }: SubmitButtonProps) {
+export default function PrimaryButton({ action, text }: PrimaryButtonProps) {
   return (
     <button
       onClick={action}
       className="w-[20.43rem] md:w-[40rem] lg:w-[30.25rem] h-[3.5rem] md:h-[5.75rem] mt-6 bg-[--purple] rounded-lg text-white"
     >
-      Submit Answer
+      {text}
     </button>
   );
 }
