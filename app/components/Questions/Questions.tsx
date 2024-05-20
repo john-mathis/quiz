52c56c39d3a25eb42ee1a1f65410b202fdc520a0
John Mathis <johnmathis98@gmail.com>
2024-05-20 19:50:24 -0400

    Questions component refactor

--- app/components/Questions/Questions.tsx
+++ app/components/Questions/Questions.tsx
@@ -1,14 +1,13 @@
 "use client";
 import { useState } from "react";
 import PrimaryButton from "../PrimaryButton/PrimaryButton";
-import Image from "next/image";
 import SelectAnswerError from "./SelectAnswerError/SelectAnswerError";
 
 interface QuestionProps {
   QUESTIONS: Array<{ question: string; options: string[]; answer: string }>;
 }
 
-const indexToLetter = (index: number) => String.fromCharCode(65 + index); // Move helper function outside
+const indexToLetter = (index: number) => String.fromCharCode(65 + index); // Helper function
 
 export default function Questions({ QUESTIONS }: QuestionProps) {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -16,19 +15,18 @@ export default function Questions({ QUESTIONS }: QuestionProps) {
   const [isOptionSelected, setIsOptionSelected] = useState(true);
   const [questionsCorrect, setQuestionsCorrect] = useState(0);
   const [submitClicked, setSubmitClicked] = useState(false);
+  const [endOfQuiz, setEndOfQuiz] = useState(false);
+
+  const currentQuestion = QUESTIONS[currentQuestionIndex];
+  const { options: currentOptions, answer: correctAnswer } = currentQuestion;
 
   const handleNext = () => {
     if (selectedOption) {
-      if (selectedOption === correctAnswer) {
-        setQuestionsCorrect(questionsCorrect + 1);
-      }
       if (currentQuestionIndex < QUESTIONS.length - 1) {
-        setCurrentQuestionIndex(currentQuestionIndex + 1);
-        setIsOptionSelected(true);
-        setSelectedOption(""); // Reset the selected option for the next question
-        setSubmitClicked(false);
+        setCurrentQuestionIndex((prev) => prev + 1);
+        resetStateForNextQuestion();
       } else {
-        // Handle the case where it's the last question
+        setEndOfQuiz(true);
       }
     } else {
       setIsOptionSelected(false);
@@ -40,62 +38,94 @@ export default function Questions({ QUESTIONS }: QuestionProps) {
       setSubmitClicked(true);
       setIsOptionSelected(true);
       if (selectedOption === correctAnswer) {
-        setQuestionsCorrect(questionsCorrect + 1);
+        setQuestionsCorrect((prev) => prev + 1);
       }
     } else {
       setIsOptionSelected(false);
     }
   };
 
-  // if the selected answer is the correct answer, highlight the correct answer with a green border and add a checkmark to the option
+  const handleOptionClick = (option: string) => {
+    if (!submitClicked) {
+      setSelectedOption(option);
+    }
+  };
 
-  // if the selected answer is the wrong answer, highlight the selected answer with a red border and add a x mark next to it while adding the checkmark next to the correct answer
+  const resetStateForNextQuestion = () => {
+    setSelectedOption("");
+    setSubmitClicked(false);
+    setIsOptionSelected(true);
+  };
 
-  // The button should change to next question after the submit an answer button is selected
+  const handleEndOfQuiz = () => {
+    setEndOfQuiz(false);
+    setCurrentQuestionIndex(0);
+    setQuestionsCorrect(0);
+    resetStateForNextQuestion();
+  };
 
-  const currentQuestion = QUESTIONS[currentQuestionIndex];
-  const currentOptions = currentQuestion.options;
-  const correctAnswer = currentQuestion.answer;
+  const renderOptions = () => {
+    return currentOptions.map((option, index) => {
+      const isSelected = selectedOption === option;
+      const isCorrect = option === correctAnswer;
+
+      let borderColorClass = "";
+      if (submitClicked) {
+        borderColorClass = isSelected
+          ? isCorrect
+            ? "border-4 border-green-500"
+            : "border-4 border-red-500"
+          : "";
+      } else if (isSelected) {
+        borderColorClass = "border-4 border-purple-500";
+      }
+
+      return (
+        <div
+          key={index}
+          onClick={() => handleOptionClick(option)}
+          className={`flex items-center pl-3 h-16 w-[20.43rem] md:w-[40rem] lg:w-[30.25rem] mb-4 bg-white dark:bg-[--grey-navy] rounded-lg cursor-pointer ${borderColorClass}`}
+        >
+          <p className="rounded flex items-center justify-center bg-[--light-grey] text-black w-[2rem] h-[2rem] mr-2">
+            {indexToLetter(index)}
+          </p>
+          <p>{option}</p>
+        </div>
+      );
+    });
+  };
 
   return (
-    <section className="mx-auto mb-6 lg:mt-10 flex flex-col lg:flex-row lg:justify-between w-[20rem] xs:w-[15rem] md:w-[40rem] lg:w-[55.5rem] xl:w-[72.5rem]">
-      <div className=" pb-4 lg:w-[30rem] h-[8.06rem] xl:ml-44 mb-8">
-        <p className="dark:text-[--light-grey] italic text-center lg:text-left">
-          Question {currentQuestionIndex + 1} of {QUESTIONS.length}
-        </p>
-        <p className="font-semibold mt-6 text-[1.25rem] md:text-[1.5rem] lg:text-[2rem] xl:text-[2.25rem] text-center lg:text-left">
-          {currentQuestion.question}
-        </p>
-      </div>
-
-      <div className="flex flex-col justify-center items-center lg:items-end lg:w-[48.5rem]">
-        {currentOptions.map((option, index) => (
-          <div
-            key={index}
-            onClick={() => setSelectedOption(option)}
-            className={`flex items-center pl-3 h-16 w-[20.43rem] md:w-[40rem] lg:w-[30.25rem] mb-4 bg-white dark:bg-[--grey-navy] rounded-lg cursor-pointer ${
-              selectedOption === option ? "border-4 border-purple-500" : ""
-            }`}
-          >
-            <p className="rounded flex items-center justify-center bg-[--light-grey] text-black w-[2rem] h-[2rem] mr-2">
-              {indexToLetter(index)}
+    <>
+      {!endOfQuiz ? (
+        <section className="mx-auto mb-6 lg:mt-10 flex flex-col lg:flex-row lg:justify-between w-[20rem] xs:w-[15rem] md:w-[40rem] lg:w-[55.5rem] xl:w-[72.5rem]">
+          <div className="pb-4 lg:w-[30rem] h-[8.06rem] xl:ml-44 mb-8">
+            <p className="dark:text-[--light-grey] italic text-center lg:text-left">
+              Question {currentQuestionIndex + 1} of {QUESTIONS.length}
             </p>
-            <p onClick={() => setIsOptionSelected(true)}>{option}1</p>
+            <p className="font-semibold mt-6 text-[1.25rem] md:text-[1.5rem] lg:text-[2rem] xl:text-[2.25rem] text-center lg:text-left">
+              {currentQuestion.question}
+            </p>
           </div>
-        ))}
 
-        {/* Displays the 'Submit Answer' button until an answer is selected and the user clicks on it. It then hides and displays the 'Next Question' button.  */}
-        {submitClicked === false && (
-          <PrimaryButton action={handleSubmit} text={"Submit Answer"} />
-        )}
+          <div className="flex flex-col justify-center items-center lg:items-end lg:w-[48.5rem]">
+            {renderOptions()}
 
-        {/* Once the 'Submit Answer' button is clicked, the 'Next Question' button is displayed. When the 'Next Question' button is clicked, it is hidden and the 'Submit Answer' button appears again.  */}
-        {submitClicked === true && (
-          <PrimaryButton action={handleNext} text={"Next Question"} />
-        )}
+            {!submitClicked ? (
+              <PrimaryButton action={handleSubmit} text="Submit Answer" />
+            ) : (
+              <PrimaryButton action={handleNext} text="Next Question" />
+            )}
 
-        {isOptionSelected === false && <SelectAnswerError />}
-      </div>
-    </section>
+            {!isOptionSelected && <SelectAnswerError />}
+          </div>
+        </section>
+      ) : (
+        <>
+          {questionsCorrect}
+          <PrimaryButton action={handleEndOfQuiz} text="Play Again" />
+        </>
+      )}
+    </>
   );
 }
