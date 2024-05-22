c7638851d0296b144c56862fe8c469173c7f9c74
John  Mathis <johnmathis98@gmail.com>
2024-05-22 11:53:53 -0400

    Imported QuizQuestionProps to help specify the type of value being passed to setQuizQuestions

--- app/components/MenuSelection/MenuSelection.tsx
+++ app/components/MenuSelection/MenuSelection.tsx
@@ -1,27 +1,28 @@
 "use client";
-import { useAppContext } from "@/app/context";
+import { useAppContext, QuizQuestionProps } from "@/app/context";
 import Selection from "../SelectButton/Selection";
 import Link from "next/link";
 import QUIZ_DATA from "../../data.json";
 
+
+
+
+
 export default function MenuSelection() {
   const { quizzes } = QUIZ_DATA;
-  const { setQuizQuestions, setSelectedTopic, selectedTopic } = useAppContext();
+  const { setQuizQuestions, setSelectedTopic} = useAppContext();
+  
 
   const quizTopics = [
     { questions: quizzes[0].questions, img: "/icon-html.svg", title: "HTML" },
     { questions: quizzes[1].questions, img: "/icon-css.svg", title: "CSS" },
-    {
-      questions: quizzes[2].questions,
-      img: "/icon-js.svg",
-      title: "Javascript",
-    },
-    {
-      questions: quizzes[3].questions,
-      img: "/icon-accessibility.svg",
-      title: "Accessibility",
-    },
+    { questions: quizzes[2].questions,img: "/icon-js.svg",title: "Javascript"},
+    { questions: quizzes[3].questions,img: "/icon-accessibility.svg",title: "Accessibility",},
   ];
+
+
+
+  
 
   return (
     <section className="flex flex-col justify-between items-center h-[18.25rem]">
@@ -30,7 +31,7 @@ export default function MenuSelection() {
           href="/QuestionPage"
           key={index}
           onClick={() => {
-            setQuizQuestions(quiz.questions);
+            setQuizQuestions(quiz.questions as QuizQuestionProps[] );
             setSelectedTopic((prevState) => ({
               ...prevState,
               title: quiz.title,
