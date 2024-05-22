9bcbed882eab17850f4b88b2b2851e5c05d30a4a
John  Mathis <johnmathis98@gmail.com>
2024-05-22 15:28:04 -0400

    Added dynamic routing to the MenuSelection Component.

--- app/components/MenuSelection/MenuSelection.tsx
+++ app/components/MenuSelection/MenuSelection.tsx
@@ -4,15 +4,11 @@ import Selection from "../SelectButton/Selection";
 import Link from "next/link";
 import QUIZ_DATA from "../../data.json";
 
-
-
-
-
 export default function MenuSelection() {
   const { quizzes } = QUIZ_DATA;
   const { setQuizQuestions, setSelectedTopic} = useAppContext();
-  
 
+// There is a more effcient way to gather this data.
   const quizTopics = [
     { questions: quizzes[0].questions, img: "/icon-html.svg", title: "HTML" },
     { questions: quizzes[1].questions, img: "/icon-css.svg", title: "CSS" },
@@ -24,7 +20,7 @@ export default function MenuSelection() {
     <section className="flex flex-col justify-between items-center h-[18.25rem]">
       {quizTopics.map((quiz, index) => (
         <Link
-          href="/QuestionPage"
+          href={`/QuestionPage/${quiz.title}`}
           key={index}
           onClick={() => {
             setQuizQuestions(quiz.questions as QuizQuestionProps[] );
@@ -33,7 +29,7 @@ export default function MenuSelection() {
               img: quiz.img,
             }));
           }}
-        >
+          >
           <Selection img={quiz.img} title={quiz.title} />
         </Link>
       ))}
