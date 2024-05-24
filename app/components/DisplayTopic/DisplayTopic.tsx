e2747adf316fbb2a0fc000e09db6c77a50226349
John Mathis <johnmathis98@gmail.com>
2024-05-24 19:00:20 -0400

    Formating fixes

--- app/components/DisplayTopic/DisplayTopic.tsx
+++ app/components/DisplayTopic/DisplayTopic.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
 
-interface DisplayTopicProps{
-    img:string,
-    title:string
+interface DisplayTopicProps {
+  img: string;
+  title: string;
 }
 
-export default function DisplayTopic({img, title}:DisplayTopicProps){
-    return(<section className="flex flex-row">
-    <Image src={img} alt={title} width={20} height={20}/>
-    <p>{title}</p>
-    </section>)
-
+export default function DisplayTopic({ img, title }: DisplayTopicProps) {
+  return (
+    <section className="flex flex-row">
+      <Image src={img} alt={title} width={20} height={20} />
+      <p>{title}</p>
+    </section>
+  );
 }
--- app/components/Questions/Questions.tsx
+++ app/components/Questions/Questions.tsx
@@ -4,7 +4,7 @@ import PrimaryButton from "../PrimaryButton/PrimaryButton";
 import SelectAnswerError from "../SelectAnswerError/SelectAnswerError";
 
 interface QuestionProps {
-  QUESTIONS: Array<{ question: string; options: []; answer: string; }>
+  QUESTIONS: Array<{ question: string; options: []; answer: string }>;
 }
 
 const indexToLetter = (index: number) => String.fromCharCode(65 + index); // Helper function
@@ -121,7 +121,7 @@ export default function Questions({ QUESTIONS }: QuestionProps) {
           </div>
         </section>
       ) : (
-        <> 
+        <>
           {questionsCorrect}
           <PrimaryButton action={handleEndOfQuiz} text="Play Again" />
         </>
