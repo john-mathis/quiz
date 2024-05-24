a1dea12ea29160479c305b501170a0b8b9ed5ae7
John Mathis <johnmathis98@gmail.com>
2024-05-23 20:28:49 -0400

    Added function to Menu Selection Component

--- app/components/MenuSelection/MenuSelection.tsx
+++ app/components/MenuSelection/MenuSelection.tsx
@@ -3,18 +3,36 @@ import { useAppContext, QuizQuestionProps } from "@/app/context";
 import Selection from "../SelectButton/Selection";
 import Link from "next/link";
 import QUIZ_DATA from "../../data.json";
+import { useRouter } from "next/navigation";
 
 export default function MenuSelection() {
   const { quizzes } = QUIZ_DATA;
-  const { setQuizQuestions, setSelectedTopic} = useAppContext();
+  const { setQuizQuestions, setSelectedTopic } = useAppContext();
+  const path = useRouter();
 
-// There is a more effcient way to gather this data.
+  // There is a more effcient way to gather this data.
   const quizTopics = [
     { questions: quizzes[0].questions, img: "/icon-html.svg", title: "HTML" },
     { questions: quizzes[1].questions, img: "/icon-css.svg", title: "CSS" },
-    { questions: quizzes[2].questions,img: "/icon-js.svg",title: "Javascript"},
-    { questions: quizzes[3].questions,img: "/icon-accessibility.svg",title: "Accessibility",},
+    {
+      questions: quizzes[2].questions,
+      img: "/icon-js.svg",
+      title: "Javascript",
+    },
+    {
+      questions: quizzes[3].questions,
+      img: "/icon-accessibility.svg",
+      title: "Accessibility",
+    },
   ];
+
+  const handleClick = (quiz: any) => {
+    setQuizQuestions(quiz.questions as QuizQuestionProps[]);
+    setSelectedTopic({
+      title: quiz.title,
+      img: quiz.img,
+    });
+  };
 
   return (
     <section className="flex flex-col justify-between items-center h-[18.25rem]">
@@ -22,14 +40,8 @@ export default function MenuSelection() {
         <Link
           href={`/QuestionPage/${quiz.title}`}
           key={index}
-          onClick={() => {
-            setQuizQuestions(quiz.questions as QuizQuestionProps[] );
-            setSelectedTopic(() => ({
-              title: quiz.title,
-              img: quiz.img,
-            }));
-          }}
-          >
+          onClick={() => handleClick(quiz)}
+        >
           <Selection img={quiz.img} title={quiz.title} />
         </Link>
       ))}
