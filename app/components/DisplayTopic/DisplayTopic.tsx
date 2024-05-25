1a95918dbcc6fdd9a2397d492af600db53496bcb
John Mathis <johnmathis98@gmail.com>
2024-05-25 19:24:13 -0400

    Added redirect from DisplayTopic component.

--- app/components/DisplayTopic/DisplayTopic.tsx
+++ app/components/DisplayTopic/DisplayTopic.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { useAppContext } from "@/app/context";
+import Link from "next/link";
 
 interface DisplayTopicProps {
   img: string;
@@ -6,10 +8,16 @@ interface DisplayTopicProps {
 }
 
 export default function DisplayTopic({ img, title }: DisplayTopicProps) {
+  const { setSelectedTopic} = useAppContext();
+
   return (
-    <section className="flex flex-row">
-      <Image src={img} alt={title} width={20} height={20} />
-      <p>{title}</p>
-    </section>
+    <Link href="/">
+      <section
+        onClick={() => setSelectedTopic({ title: "", img: "" })}
+        className="flex flex-row">
+        <Image src={img} alt={title} width={20} height={20} />
+        <p>{title}</p>
+      </section>
+    </Link>
   );
 }
