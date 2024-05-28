668d30d9d6b2ce78d9b1202b57273a0e96291fdd
John Mathis <johnmathis98@gmail.com>
2024-05-28 17:01:41 -0400

    Added QuizComplete component.

---
+++ app/components/QuizComplete/QuizComplete.tsx
@@ -0,0 +1,13 @@
+export default function QuizComplete() {
+  return (
+    <section>
+      <div>
+        <p>
+          Quiz completed <span>You scored...</span>
+        </p>
+      </div>
+
+      <div></div>
+    </section>
+  );
+}
