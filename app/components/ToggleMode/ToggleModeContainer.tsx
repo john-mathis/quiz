8a9b6b50350e8120308c410416a400c638344daf
John Mathis <johnmathis98@gmail.com>
2024-05-30 17:08:56 -0400

    Added useRouter based navigation into the ToggleMenuContainer component.

--- app/components/ToggleMode/ToggleModeContainer.tsx
+++ app/components/ToggleMode/ToggleModeContainer.tsx
@@ -1,34 +1,43 @@
-"use client"
+"use client";
 
 import { useAppContext } from "@/app/context";
 import DisplayTopic from "../DisplayTopic/DisplayTopic";
 import ToggleMode from "./ToggleMode";
-import { useState,useEffect } from "react";
-
-
+import { useState, useEffect } from "react";
+import { useRouter, usePathname } from "next/navigation";
 
 export default function ToggleModeContainer() {
-    const [objectIsEmpty, setObjectIsEmpty] = useState(true);
-    const { selectedTopic } = useAppContext();
+  const [objectIsEmpty, setObjectIsEmpty] = useState(true);
+  const { selectedTopic } = useAppContext();
+  const router = useRouter();
+  const pathName = usePathname();
 
+  useEffect(() => {
+    const isObjectEmpty = () => {
+      if (selectedTopic.title === "") {
+        //
+        router.push("/");
+        setObjectIsEmpty(true);
+      } else {
+        router.push(`QuestionPage/${selectedTopic.title}`);
+        setObjectIsEmpty(false);
+      }
+    };
+    isObjectEmpty();
+  }, [router, selectedTopic]);
 
-
-    useEffect(() => {
-        const isObjectEmpty = () => {
-            if (selectedTopic.title ==="") {
-                setObjectIsEmpty(true);
-            } else {
-                setObjectIsEmpty(false);
-            }
-        };
-
-        isObjectEmpty();
-    }, [selectedTopic]);
-    
-    return (
-        <section className={`flex ${objectIsEmpty ? "justify-end" : "justify-between"} mx-auto pt-4 w-[20.43rem] md:w-[40rem] md:pt-8 lg:w-[55.5rem] xl:w-[72.5rem]`}>
-            {objectIsEmpty ? "" : <DisplayTopic img={selectedTopic.img} title={selectedTopic.title} />}
-            <ToggleMode />
-        </section>
-    );
+  return (
+    <section
+      className={`flex ${
+        objectIsEmpty ? "justify-end" : "justify-between"
+      } mx-auto pt-4 w-[20.43rem] md:w-[40rem] md:pt-8 lg:w-[55.5rem] xl:w-[72.5rem]`}
+    >
+      {objectIsEmpty ? (
+        ""
+      ) : (
+        <DisplayTopic img={selectedTopic.img} title={selectedTopic.title} />
+      )}
+      <ToggleMode />
+    </section>
+  );
 }
