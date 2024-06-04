925a94d690221bce5b3acb8f5df4bb1db712e75a
John  Mathis <johnmathis98@gmail.com>
2024-06-04 16:32:59 -0400

    Formating fixes

--- app/components/QuizComplete/QuizComplete.tsx
+++ app/components/QuizComplete/QuizComplete.tsx
@@ -8,7 +8,8 @@ interface QuizCompleteProps {
 }
 export default function QuizComplete({questionsCorrect, topic}:QuizCompleteProps) {
   return (
-    <section className=" mt-8 xs:w-[18rem] w-[20.43rem]">
+    <section className="xl-flex xl-flex-row mt-8 xs:w-[18rem] w-[20.43rem] md:w-[40rem] lg:w-[30.25rem] xl:w-[35rem]">
+
       <div className="text-4xl">
         <p className="font-extralight">
           Quiz completed 
@@ -17,15 +18,12 @@ export default function QuizComplete({questionsCorrect, topic}:QuizCompleteProps
       </div>
 
       <div className="flex flex-col items-center justify-center mt-8 rounded-xl bg-white dark:bg-[--grey-navy] h-[15.12rem]">
+        <div className="flex justify-center mb-8">
+        <Image src={topic.img} alt={""} width={20}  height={20}/>
+        <p className="text-[1.1rem]">{topic.title}</p>
+       </div>
 
-
-      <div className="flex justify-center mb-8">
-      <Image src={topic.img} alt={""} width={20}  height={20}/>
-      <p className="text-[1.1rem]">{topic.title}</p>
-      </div>
-
-
-      <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center justify-center">
         <p className="text-7xl">{questionsCorrect}</p> 
         <p>out of 10</p>
         </div>
