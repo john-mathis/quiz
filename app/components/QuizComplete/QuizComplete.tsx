f158d25f01493df20da110fe8015ae39f5a92070
John  Mathis <johnmathis98@gmail.com>
2024-06-04 15:47:04 -0400

    Brought styling and logic into the QuizComplete component.

--- app/components/QuizComplete/QuizComplete.tsx
+++ app/components/QuizComplete/QuizComplete.tsx
@@ -1,13 +1,39 @@
-export default function QuizComplete() {
+import Image from "next/image";
+
+
+
+interface QuizCompleteProps {
+  questionsCorrect: number;
+  topic: {title:string, img:string}
+}
+export default function QuizComplete({questionsCorrect, topic}:QuizCompleteProps) {
   return (
-    <section>
-      <div>
-        <p>
-          Quiz completed <span>You scored...</span>
+    <section className=" mt-8 xs:w-[18rem] w-[20.43rem]">
+      <div className="text-4xl">
+        <p className="font-extralight">
+          Quiz completed 
         </p>
+        <p className="font-semibold">You scored...</p>
       </div>
 
-      <div></div>
+      <div className="flex flex-col items-center justify-center mt-8 rounded-xl bg-white dark:bg-[--grey-navy] h-[15.12rem]">
+
+
+      <div className="flex justify-center mb-8">
+      <Image src={topic.img} alt={""} width={20}  height={20}/>
+      <p className="text-[1.1rem]">{topic.title}</p>
+      </div>
+
+
+      <div className="flex flex-col items-center justify-center">
+        <p className="text-7xl">{questionsCorrect}</p> 
+        <p>out of 10</p>
+        </div>
+
+
+      </div>
+
+
     </section>
   );
 }
