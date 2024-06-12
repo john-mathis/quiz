988a5d6d861c3fb3e38a7ce0914af54a6feba702
John  Mathis <johnmathis98@gmail.com>
2024-06-12 11:19:26 -0400

    Removed unused lines of code

--- app/components/ToggleMode/ToggleModeContainer.tsx
+++ app/components/ToggleMode/ToggleModeContainer.tsx
@@ -5,20 +5,19 @@ import DisplayTopic from "../DisplayTopic/DisplayTopic";
 import ToggleMode from "./ToggleMode";
 import { useState, useEffect } from "react";
 import { useRouter, usePathname } from "next/navigation";
-import path from "path";
+
 
 export default function ToggleModeContainer() {
   const [objectIsEmpty, setObjectIsEmpty] = useState(true);
   const { selectedTopic } = useAppContext();
   const router = useRouter();
-  const pathName = usePathname()
+  
 
   useEffect(() => {
     if (selectedTopic.title === ""  ) {
       router.push("/");
       setObjectIsEmpty(true);
     } else {
-      // router.push(`QuestionPage/${selectedTopic.title}`);
       setObjectIsEmpty(false);
     }
     
